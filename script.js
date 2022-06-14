4b0b08650b664dd267d1aa0e3ef0e300eb5797cb
beatriz-cs <106784551+beatriz-cs@users.noreply.github.com>
2022-06-14 16:27:34 -0300

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+for (let count = 1; count <= 10; count ++){
+    console.log("loop com for", count)
+}
+
+
+for (let impar = 1; impar <=100; impar+=2){
+    console.log("ímpares",impar)
+}
+
+let count = 1
+
+for (let par = 0; par <=100; par+=2){
+    console.log("pares", par)
+}
+
+let par = 0
+
+while(10 >= count){
+    console.log("loop com while", count);
+    count ++;
+} 
+
+
+
+
